cd13ae4b99f5f6451e5fc521b354985338588491
Nhazz <manjirokys@gmail.com>
2026-09-25 19:55:47 +0700

    fixed a cartApi

--- frontend/src/Services/cartApi.js
+++ frontend/src/Services/cartApi.js
@@ -21,7 +21,36 @@ export const createCart = async (
     );
 
     console.log(
-        "Backend cart response:",
+        "Backend created cart:",
+        response.data
+    );
+
+    return response.data;
+};
+
+export const updateCart = async (
+    cartId,
+    productId,
+    productVariantId,
+    quantity,
+    cartToken
+) => {
+    const response = await api.put(
+        `/api/cart/${cartId}`,
+        {
+            productId,
+            productVariantId,
+            quantity,
+        },
+        {
+            headers: {
+                "X-Cart-Token": cartToken || "",
+            },
+        }
+    );
+
+    console.log(
+        "Backend updated cart:",
         response.data
     );
 
